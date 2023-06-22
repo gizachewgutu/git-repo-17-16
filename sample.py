9e1e7431d7b1c75eec6f4738dc25bae714ac26df
Gutu <gizachoo@gmail.com>
2023-06-22 07:23:07 +0000

    to see the conflict between bug-fix and main

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@ a=3
 b=8
 c=6
 d=3
-sum =a+b+c+d
-print "sum of four numbers is" sum
-print "the sum of five numbers result""
+e=2
+sum =a+b+c+d+e
+print "sum of 5 numbers is" sum
+print "the sum of 5 numbers result""
