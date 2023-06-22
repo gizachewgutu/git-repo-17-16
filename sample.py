28981e6335b2ad96aa9ede7812dc824717496b44
Jane <66804912+gizachewgutu@users.noreply.github.com>
2023-06-22 02:39:06 -0500

    Update sample.py
    developer two make change in the GitHub or remote.

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ a=3
 b=8
 c=6
 d=3
-e=77
+e=90
 sum =a+b+c+d+e
-print "sum of five  numbers is" sum
-print "the sum of five numbers result"
+print "sum of 6  numbers is" sum
+print "the sum of 6 numbers result"
